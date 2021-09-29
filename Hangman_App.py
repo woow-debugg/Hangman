80831be03fc28bca19915a6a5fb6034108da26f0
woow-debugg <62289822+woow-debugg@users.noreply.github.com>
2021-09-29 08:01:08 -0600

    Update Hangman_App.py
    The words have been moved into a separate file "hangman_words.py" and the code has improved commenting.

--- Hangman_App.py
+++ Hangman_App.py
@@ -1,294 +1,91 @@
-from tkinter import *
-from PIL import ImageTk,Image
-from tkinter import messagebox
+"""
+PROGRAM INFORMATION
+
+29 September 2021
+Programmer: Tristin Hinojosa
+
+filename: Hangman_App.py
+
+SUMMARY:
+This program lets the user play the game of hangman using the tkinter GUI
+
+"""
+from tkinter import messagebox, Tk, Label, END, Entry, Button
+from PIL import ImageTk, Image
+from hangman_words import words
 import random
 root = Tk()
 root.wm_geometry('230x300')
-root.title('THE GAME OF HANGMAN')
-words = ['abruptly',
-'absurd',
-'abyss',
-'affix',
-'askew',
-'avenue',
-'awkward',
-'axiom',
-'azure',
-'bagpipes',
-'bandwagon',
-'banjo',
-'bayou',
-'beekeeper',
-'bikini',
-'blitz',
-'blizzard',
-'boggle',
-'bookworm',
-'boxcar',
-'boxful',
-'buckaroo',
-'buffalo',
-'buffoon',
-'buxom',
-'buzzard',
-'buzzing',
-'buzzwords',
-'caliph',
-'cobweb',
-'cockiness',
-'croquet',
-'crypt',
-'curacao',
-'cycle',
-'daiquiri',
-'dirndl',
-'disavow',
-'dizzying',
-'duplex',
-'dwarves',
-'embezzle',
-'equip',
-'espionage',
-'euouae',
-'exodus',
-'faking',
-'fishhook',
-'fixable',
-'fjord',
-'flapjack',
-'flopping',
-'fluffiness',
-'flyby',
-'foxglove',
-'frazzled',
-'frizzled',
-'fuchsia',
-'funny',
-'gabby',
-'galaxy',
-'galvanize',
-'gazebo',
-'giaour',
-'gizmo',
-'glowworm',
-'glyph',
-'gnarly',
-'gnostic',
-'gossip',
-'grogginess',
-'haiku',
-'haphazard',
-'hyphen',
-'iatrogenic',
-'icebox',
-'injury',
-'ivory',
-'ivy',
-'jackpot',
-'jaundice',
-'jawbreaker',
-'jaywalk',
-'jazziest',
-'jazzy',
-'jelly',
-'jigsaw',
-'jinx',
-'jiujitsu',
-'jockey',
-'jogging',
-'joking',
-'jovial',
-'joyful',
-'juicy',
-'jukebox',
-'jumbo',
-'kayak',
-'kazoo',
-'keyhole',
-'khaki',
-'kilobyte',
-'kiosk',
-'kitsch',
-'kiwifruit',
-'klutz',
-'knapsack',
-'larynx',
-'lengths',
-'lucky',
-'luxury',
-'lymph',
-'marquis',
-'matrix',
-'megahertz',
-'microwave',
-'mnemonic',
-'mystify',
-'naphtha',
-'nightclub',
-'nowadays',
-'numbskull',
-'nymph',
-'onyx',
-'ovary',
-'oxidize',
-'oxygen',
-'pajama',
-'peekaboo',
-'phlegm',
-'pixel',
-'pizazz',
-'pneumonia',
-'pneumonoultramicroscopicsilicovolcanoconiosis',
-'polka',
-'pshaw',
-'psyche',
-'puppy',
-'puzzling',
-'quartz',
-'queue',
-'quips',
-'quixotic',
-'quiz',
-'quizzes',
-'quorum',
-'razzmatazz',
-'rhubarb',
-'rhythm',
-'rickshaw',
-'schnapps',
-'scratch',
-'shiv',
-'snazzy',
-'sphinx',
-'spritz',
-'squawk',
-'staff',
-'strength',
-'strengths',
-'stretch',
-'stronghold',
-'stymied',
-'subway',
-'swivel',
-'syndrome',
-'thriftless',
-'thumbscrew',
-'topaz',
-'transcript',
-'transgress',
-'transplant',
-'triphthong',
-'twelfth',
-'twelfths',
-'unknown',
-'unworthy',
-'unzip',
-'uptown',
-'vaporize',
-'vixen',
-'vodka',
-'voodoo',
-'vortex',
-'voyeurism',
-'walkway',
-'waltz',
-'wave',
-'wavy',
-'waxy',
-'wellspring',
-'wheezy',
-'whiskey',
-'whizzing',
-'whomever',
-'wimpy',
-'witchcraft',
-'wizard',
-'woozy',
-'wristwatch',
-'wyvern',
-'xylophone',
-'yachtsman',
-'yippee',
-'yoked',
-'youthful',
-'yummy',
-'zephyr',
-'zigzag',
-'zigzagging',
-'zilch',
-'zipper',
-'zodiac',
-'zombie'
-]
+root.title('HANGMAN')
+
+
 global secret_word
 secret_word = random.choice(words)
-#get the dashes for the secret word
+
 dash = []
-guesdinc = []
-guesCOR = []
-def dashes():
-    for i in range(len(secret_word)):
-        dash.append('-')
-dashes()
-#IMAGES FOR THE GAME BEING IMPORTED HERE FOR USE
-my_img0 = ImageTk.PhotoImage(Image.open("hangman_Image0.jpg"))
-my_img1 = ImageTk.PhotoImage(Image.open("hangman_Image1.jpg"))
-my_img2 = ImageTk.PhotoImage(Image.open("hangman_Image2.jpg"))
-my_img3 = ImageTk.PhotoImage(Image.open("hangman_Image3.jpg"))
-my_img4 = ImageTk.PhotoImage(Image.open("hangman_Image4.jpg"))
-my_img5 = ImageTk.PhotoImage(Image.open("hangman_Image5.jpg"))
-my_img6 = ImageTk.PhotoImage(Image.open("hangman_Image6.jpg"))
-my_img7 = ImageTk.PhotoImage(Image.open("hangman_Image7.jpg"))
-my_img8 = ImageTk.PhotoImage(Image.open("hangman_Image8.jpg"))
-my_img9 = ImageTk.PhotoImage(Image.open("hangman_Image9.jpg"))
-my_img10 = ImageTk.PhotoImage(Image.open("hangman_Image10.jpg"))
+guessed_incorrect = []
+guessed_correct = []
+
+
+#IMAGES FOR THE GAME BEING IMPORTED HERE FOR THE GAME
+my_img0  = ImageTk.PhotoImage(Image.open("Pictures\hangman_Image0.jpg"))
+my_img1  = ImageTk.PhotoImage(Image.open("Pictures\hangman_Image1.jpg"))
+my_img2  = ImageTk.PhotoImage(Image.open("Pictures\hangman_Image2.jpg"))
+my_img3  = ImageTk.PhotoImage(Image.open("Pictures\hangman_Image3.jpg"))
+my_img4  = ImageTk.PhotoImage(Image.open("Pictures\hangman_Image4.jpg"))
+my_img5  = ImageTk.PhotoImage(Image.open("Pictures\hangman_Image5.jpg"))
+my_img6  = ImageTk.PhotoImage(Image.open("Pictures\hangman_Image6.jpg"))
+my_img7  = ImageTk.PhotoImage(Image.open("Pictures\hangman_Image7.jpg"))
+my_img8  = ImageTk.PhotoImage(Image.open("Pictures\hangman_Image8.jpg"))
+my_img9  = ImageTk.PhotoImage(Image.open("Pictures\hangman_Image9.jpg"))
+my_img10 = ImageTk.PhotoImage(Image.open("Pictures\hangman_Image10.jpg"))
 imglst = [my_img0, my_img2, my_img3, my_img4, my_img5, my_img6, my_img7, my_img8, my_img9, my_img10, my_img1]
+
+#This function converts the "secret_word" into dashes,
+# in order for the user to see the placement of their correct guesses in the secret word.
+def dashes():
+    for letter in range(len(secret_word)):
+        dash.append('-')
+
+dashes()
+
+
+#This function is used to switch the images depending on the status of the game
 def switchIMG():
     global y
     global v
     myimage0.grid_forget()
-    y = len(guesdinc)
+    y = len(guessed_incorrect)
     v = y - 1
     if y == 1:
         newIMG = Label(image=my_img0).grid(row=1, column=0, columnspan=3)
-    elif y == 2:
-        newIMG = Label(image=imglst[v]).grid(row=1, column=0, columnspan=3)
-    elif y == 3:
-        newIMG = Label(image=imglst[v]).grid(row=1, column=0, columnspan=3)
-    elif y == 4:
-        newIMG = Label(image=imglst[v]).grid(row=1, column=0, columnspan=3)
-    elif y == 5:
-        newIMG = Label(image=imglst[v]).grid(row=1, column=0, columnspan=3)
-    elif y == 6:
-        newIMG = Label(image=imglst[v]).grid(row=1, column=0, columnspan=3)
-    elif y == 7:
-        newIMG = Label(image=imglst[v]).grid(row=1, column=0, columnspan=3)
-    elif y == 8:
-        newIMG = Label(image=imglst[v]).grid(row=1, column=0, columnspan=3)
-    elif y == 9:
-        newIMG = Label(image=imglst[v]).grid(row=1, column=0, columnspan=3)
-    elif y == 10:
-        newIMG = Label(image=imglst[v]).grid(row=1, column=0, columnspan=3)
-    elif y == 11:
-        newIMG = Label(image=imglst[v]).grid(row=1, column=0, columnspan=3)
     else:
-        newIMG = Label(image=my_img0).grid(row=1, column=0, columnspan=3)
+        newIMG = Label(image=imglst[v]).grid(row=1, column=0, columnspan=3)
+
+
+#This function is used when a letter is guessed incorrectly
+#If the user has incorrectly guessed 10 or more times they lose the game. 
 def getletterwrong():
     x = e.get()
-    guesdinc.append(x)
+    guessed_incorrect.append(x)
     switchIMG()
     #print the new list on the screen
-    label4 = Label(root, text=(str(''.join(guesdinc)))).grid(row=5, column=0, columnspan=3)
+    label4 = Label(root, text=(str(''.join(guessed_incorrect)))).grid(row=5, column=0, columnspan=3)
     e.delete(0, 'end')
-    if len(guesdinc) >= 10:
-        answer = messagebox.askyesno((str(secret_word)), 'You killed a man, his blood is on your hands...would you like to try again? ')
+    if len(guessed_incorrect) >= 10:
+        answer = messagebox.askyesno("REST IN PEACE", f'''You killed a man, his blood is on your hands
+                                    ...would you like to try again? The secret word was {str(secret_word)}''')
         if answer == True:
             resetallLose()
         else:
             root.quit()
+
+
+#This function is used when a letter is guessed correctly
+#If the user has correctly guessed the enough letters to match the "secret_word" they win the game. 
 def getletterright():
     x = e.get()
-    guesCOR.append(x)
+    guessed_correct.append(x)
     label3 = Label(root, text=(''.join(dash))).grid(row=3, column= 0, columnspan=3)
     for i in range(len(secret_word)):
         if secret_word[i] == x:
@@ -303,6 +100,21 @@ def getletterright():
         label3 = Label(root, text=(''.join(dash))).grid(row=3, column=0, columnspan=3)
         e.delete(0, 'end')
     e.delete(0, END)
+
+
+
+#This function will display the basic layout of the screen after the user has restarted the game, win or lose.
+def BasicScreenLayout():
+    label1 = Label(root, text='                           ').grid(row=2, column=0, columnspan=3)
+    label1 = Label(root, text='Your secret word is ' + (str(len(secret_word))) + ' letters long').grid(row=2, column= 0, columnspan=3)
+    label3 = Label(root, text='                           ').grid(row=3, column=0, columnspan=3)
+    label3 = Label(root, text=(''.join(dash))).grid(row=3, column=0, columnspan=3)
+    label4 = Label(root, text='                           ').grid(row=5, column=0, columnspan=3)
+    label4 = Label(root, text=(str(''.join(guessed_incorrect)))).grid(row=5, column=0, columnspan=3)
+
+
+# This function resets the game if the user wins, it resets y and v.
+# By resetting y and v it allows all the mistakes and progress of the last game to be places at 0.
 def resetallwin():
     global y
     global v
@@ -311,17 +123,16 @@ def resetallwin():
     v = 0
     secret_word = ()
     dash.clear()
-    guesdinc.clear()
-    guesCOR.clear()
+    guessed_incorrect.clear()
+    guessed_correct.clear()
     secret_word = random.choice(words)
     dashes()
-    label1 = Label(root, text='                           ').grid(row=2, column=0, columnspan=3)
-    label1 = Label(root, text='Your secret word is ' + (str(len(secret_word))) + ' letters long').grid(row=2, column=0, columnspan=3)
-    label3 = Label(root, text='                           ').grid(row=3, column=0, columnspan=3)
-    label3 = Label(root, text=(''.join(dash))).grid(row=3, column=0, columnspan=3)
-    label4 = Label(root, text='                           ').grid(row=5, column=0, columnspan=3)
-    label4 = Label(root, text=(str(''.join(guesdinc)))).grid(row=5, column=0, columnspan=3)
+    BasicScreenLayout()
+
     myimage0 = Label(image=my_img0).grid(row=1, column=0, columnspan=3)
+
+# This function resets the game if the user loses, it resets y and v.
+# By resetting y and v it allows all the mistakes and progress of the last game to be places at 0.
 def resetallLose():
     global y
     global v
@@ -330,58 +141,73 @@ def resetallLose():
     v = 0
     secret_word = ()
     dash.clear()
-    guesdinc.clear()
-    guesCOR.clear()
+    guessed_incorrect.clear()
+    guessed_correct.clear()
     secret_word = random.choice(words)
     dashes()
-    label1 = Label(root, text='                           ').grid(row=2, column=0, columnspan=3)
-    label1 = Label(root, text='Your secret word is ' + (str(len(secret_word))) + ' letters long').grid(row=2, column= 0, columnspan=3)
-    label3 = Label(root, text='                           ').grid(row=3, column=0, columnspan=3)
-    label3 = Label(root, text=(''.join(dash))).grid(row=3, column=0, columnspan=3)
-    label4 = Label(root, text='                           ').grid(row=5, column=0, columnspan=3)
-    label4 = Label(root, text=(str(''.join(guesdinc)))).grid(row=5, column=0, columnspan=3)
+    BasicScreenLayout()
     myimage0 = Label(image=my_img1).grid(row=1, column=0, columnspan=3)
+
+
 #function that lets the user press the enter key to submit letters to the program
 #letter is just used to show that your are passing information from e through decideletter()
 def onEnter(letter):
     decideletter()
+
+
+
+#This function decides if the input is a lowercase letter, all other input will result in an error message.
 def decideletter():
     x = e.get()
     if len(x) > 1:
         e.delete(0, END)
         messagebox.showinfo('BIG OOF', 'You must enter only one letter at a time.')
-    elif x in guesdinc:
+    elif x in guessed_incorrect:
         e.delete(0, END)
         messagebox.showinfo('BIG OOF', 'You have already guessed that letter incorrectly.')
-    elif x in guesCOR:
+    elif x in guessed_correct:
         e.delete(0, END)
         messagebox.showinfo('BIG OOF', 'You have already guessed that letter correctly.')
     elif x.isupper() == True:
         e.delete(0, END)
-        messagebox.showinfo('BIG OOF', 'You must only enter lowercase letters')
+        messagebox.showinfo('BIG OOF', 'You must only enter lowercase letters!')
+    elif x.isdigit() == True:
+        e.delete(0, END)
+        messagebox.showinfo('BIG OOF', 'You must only enter letters, digits are not allowed!')
     elif x in secret_word:
         getletterright()
     elif x not in secret_word:
         getletterwrong()
     label3 = Label(root, text=(''.join(dash))).grid(row=3, column= 0, columnspan=3)
-#all the things on the screen in order of the screen
+
+    
+#THIS STARTS THE ORGANIZATION OF THE SCREEN AS SHOWN TO THE USER
 label0 = Label(root, text='Welcome to Hangman').grid(row=0, column=0, columnspan=3)
-#first hangman image that the user can see
-my_img0 = ImageTk.PhotoImage(Image.open("hangman_Image0.jpg"))
-myimage0 = Label(image=my_img0)
+
+
+#First hangman image that the user will see, the cover photo.
+first_image = ImageTk.PhotoImage(Image.open("Pictures\hangman_Image0.jpg"))
+myimage0 = Label(image=first_image)
 myimage0.grid(row=1, column=0, columnspan=3)
 label1 = Label(root, text='Your secret word is ' + (str(len(secret_word))) + ' letters long')
 label1.grid(row=2, column= 0, columnspan=3)
-#the text of the word
+
+
+#Displays the word to be guessed and the incorrect guesses of the user.
 label3 = Label(root, text=(str(''.join(dash)))).grid(row=3, column=0, columnspan=3)
-label4 = Label(root, text=(str(''.join(guesdinc)))).grid(row=5, column=0, columnspan=3)
-#what the user has entered
+label4 = Label(root, text=(str(''.join(guessed_incorrect)))).grid(row=5, column=0, columnspan=3)
+
+
+#Displays what the user has entered.
 guesdlabel = Label(root, text='So far you\'ve entered...').grid(row=4, column=0, columnspan=3)
-#where the letters are entered
+
+
+#This sets up the input box so the user can enter characters.
 e = Entry(root)
 e.bind('<Return>', onEnter)
 e.grid(row=6, column=0, columnspan=3)
 e.insert(0, '???')
 btn = Button(root, text='enter', command=decideletter)
 btn.grid(row=7, column=0, columnspan=3)
+print(f'{"_" * 35}The Game of Hangman is currently running{"_" * 35}')
 root.mainloop()
